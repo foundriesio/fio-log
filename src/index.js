feb08cccd513cf630b42199ff49241526f25edee
Milo Casagrande <milo@foundries.io>
2021-04-27 10:38:18 +0200

    fix: correct multistream support
      * Add levels and `dedupe` option to just log
        in the correct stream.
    
    Signed-off-by: Milo Casagrande <milo@foundries.io>

--- src/index.js
+++ src/index.js
@@ -4,7 +4,19 @@ import pinoms from 'pino-multi-stream';
 const { NODE_ENV } = process.env;
 const isProd = NODE_ENV === 'production';
 
-const streams = [{ stream: process.stdout, level: isProd ? 'info' : 'debug' }];
+const streams = [{ stream: process.stdout }];
+const options = {
+  levels: {
+    silent: Infinity,
+    fatal: 60,
+    error: 50,
+    warn: 50,
+    info: 30,
+    debug: 20,
+    trace: 10,
+  },
+  dedupe: true,
+};
 
 if (isProd) {
   streams.push({ level: 'error', stream: process.stderr });
@@ -12,9 +24,9 @@ if (isProd) {
 
 const log = pino(
   {
-    level: isProd ? 'info' : 'debug'
+    level: isProd ? 'info' : 'trace',
   },
-  pinoms.multistream(streams)
+  pinoms.multistream(streams, options)
 );
 
 export default log;
